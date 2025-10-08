c89b38447fdfedeccffcb5517d8304975208259f
Jonathan Nascimento Moletta <jonathan-moletta@PPIRATINI.INTRA.RS.GOV.BR>
2025-10-08 17:10:58 -0300

    subindo projeto para executar na tv da manutenção

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MaintenanceDashboard from "./MaintenanceDashboard";
 import "./dtic-index.css";
+import "./overflow-hotfix.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
